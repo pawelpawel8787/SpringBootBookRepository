7fef59dbcc192600aa2d64aac17963a490f03131
PawelOrzel <bassista@interia.pl>
2018-01-13 15:24:27 +0100

    add POST method into BookController

--- src/main/java/com/mycompany/controller/BookController.java
+++ src/main/java/com/mycompany/controller/BookController.java
@@ -2,12 +2,10 @@ package com.mycompany.controller;
 
 import com.mycompany.model.Book;
 import com.mycompany.service.BookServiceImpl;
+import javafx.geometry.Pos;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -31,6 +29,11 @@ public class BookController {
         }
 
         return ResponseEntity.ok(book);
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public void saveBook(@RequestBody Book book){
+        bookService.createBook(book);
     }
 
 
